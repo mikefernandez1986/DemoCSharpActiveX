aaa1354137c2f421822e680663c6b9679af35a10
Dell <Dell@DELL-PC>
2019-09-08 01:56:35 +0530

    MY Applicationj

---
+++ DemoCSharpActiveX/HelloWord.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+
+namespace DemoCSharpActiveX
+
+{
+
+    /// <summary>
+
+    /// Demo HelloWorld class
+
+    /// </summary>
+
+    [ProgId("DemoCSharpActiveX.HelloWorld")]
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+
+    [Guid("EB226149-BEBE-473B-AF07-8007A7A57C0A")]
+
+    [ComVisible(true)]
+
+    public class HelloWord
+
+    {
+
+        [ComVisible(true)]
+
+        public String SayHello()
+
+        {
+
+            return "Hello World!";
+
+        }
+
+    }
+
+}
